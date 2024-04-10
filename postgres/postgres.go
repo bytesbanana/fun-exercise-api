8fd25ee63139b298a21f6dac60e04d850faec037
bytesbanana <saharat.paynok@gmail.com>
2024-04-10 22:27:37 +0700

    using environment variables

--- postgres/postgres.go
+++ postgres/postgres.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -13,8 +15,22 @@ type Postgres struct {
 }
 
 func New() (*Postgres, error) {
+	host := os.Getenv("DB_HOST")
+	port, err := strconv.Atoi(os.Getenv("DB_PORT"))
+	if err != nil {
+		log.Fatalf("failed to convert PORT: %v", err)
+	}
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	databaseSource := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable", "localhost", 5432, "root", "password", "wallet")
+		"password=%s dbname=%s sslmode=disable",
+		host,
+		port,
+		user,
+		password,
+		dbname)
 	db, err := sql.Open("postgres", databaseSource)
 	if err != nil {
 		log.Fatal(err)
