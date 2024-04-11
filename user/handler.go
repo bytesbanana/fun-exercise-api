76c83d43038ccc92606004ce8014b17b641a8444
bytesbanana <saharat.paynok@gmail.com>
2024-04-11 16:21:03 +0700

    add find wallets by user id handler

---
+++ user/handler.go
@@ -0,0 +1,50 @@
+package user
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/KKGo-Software-engineering/fun-exercise-api/wallet"
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct {
+	store Storer
+}
+
+type Storer interface {
+	WalletsByUserID(userId int) ([]wallet.Wallet, error)
+}
+
+func New(db Storer) *Handler {
+	return &Handler{store: db}
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+// WalletByUserIdHandler
+//
+//	@Summary		Get all wallets by user id
+//	@Description	Get all wallets by user id
+//	@Router			/api/v1/users/:id/wallets [get]
+//	@Tags			users, wallet
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	Wallet
+//	@Failure		400	{object}	Err
+//	@Failure		500	{object}	Err
+func (h *Handler) WalletByUserIdHandler(c echo.Context) error {
+	pUserId := c.Param("id")
+	userId, err := strconv.Atoi(pUserId)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, Err{Message: "Invalid user id"})
+	}
+
+	wallets, err := h.store.WalletsByUserID(userId)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+	}
+	return c.JSON(http.StatusOK, wallets)
+}
